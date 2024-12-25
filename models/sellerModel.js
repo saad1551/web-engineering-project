a7fe541a8bbd5c37b7141f3f549521bc5f5923fc
saad1551 <saad.ashraf.9094@gmail.com>
2024-12-25 12:48:44 +0500

    make seller model

---
+++ models/sellerModel.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const SellerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email'],
+        unique: true,
+        match: [
+            /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+            'Please add a valid email'
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: 6,
+        select: false
+    },
+    // Date of Birth
+    dateOfBirth: { 
+        type: Date, 
+        required: true,
+        validate: {
+            validator: function(value) {
+            // Ensure the date is in the past
+            return value < new Date();
+            },
+            message: 'Date of birth must be in the past.',
+        },
+    },
+    district: {
+        type: String,
+        required: [true, 'Please add a district']
+    },
+    division: {
+        type: String,
+        required: [true, 'Please add a division']
+    },
+    province: {
+        type: String,
+        required: [true, 'Please add a province']
+    },
+    phoneNumber: {
+        type: String,
+        // make sure it is an eleven digit phone number
+        match: [/^\d{11}$/]
+    }
+})
+
+// add middleware to hash the password before saving
+SellerSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+})
+
+const SellerModel = mongoose.model('Seller', SellerSchema);
+
+module.exports = SellerModel;
